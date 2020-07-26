9c34bb116ce1ee0aeec59fee35e0972fd151cb6f
Ivan <vanyapandoctor@gmail.com>
2020-07-26 11:35:38 +0300

    delete clock

--- js/script.js
+++ js/script.js
@@ -18,22 +18,4 @@ window.addEventListener('DOMContentLoaded', function () {
             },1000);
         });
     });
-
-    // CLOCK
-
-    const deg = 6,
-        hr = document.querySelector('#hr'),
-        mn = document.querySelector('#mn'),
-        sc = document.querySelector('#sc');
-
-    setInterval(() => {
-        let day = new Date(),
-            hh = day.getHours() * 30,
-            mm = day.getMinutes() * deg,
-            ss = day.getSeconds() * deg;
-
-        hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-        mn.style.transform = `rotateZ(${mm}deg)`;
-        sc.style.transform = `rotateZ(${ss}deg)`;
-    });
 });
